703caf43c07b05adbcbbf6be5f09f8bcfc42ce9b
janinquisitor <jancarlos0195@gmail.com>
2021-06-24 10:44:56 -0400

    Add review routes add data modeling with mongoose

---
+++ controllers/reviewController.js
@@ -0,0 +1,5 @@
+const AppError = require("./../utils/appError");
+const Review = require("./../models/reviewModel");
+const catchAsync = require("./../utils/catchAsync");
+
+exports.getReviews = (req, res, next) => {};
---
+++ routes/reviewRotes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const controller = require("../controllers/reviewController");
+const auth = require("../controllers/authController");
+
+const router = express.Router();
